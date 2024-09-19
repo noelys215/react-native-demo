1e490f942a93b0ebc60a9c0125b3882201050d9d
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-09-19 19:48:32 -0400

    refactor: - Create AppGradient component for consistent gradient styling - Create Content component for safe area and padding

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
 import { View, Text, ImageBackground } from 'react-native';
 import React from 'react';
 import beachImage from '@/assets/meditation-images/beach.webp';
-import { LinearGradient } from 'expo-linear-gradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import CustomButton from '@/components/CustomButton';
+import { useRouter } from 'expo-router';
+import AppGradient from '@/components/AppGradient';
 
 const App = () => {
+	const router = useRouter();
+
 	return (
 		<View className="flex-1">
 			<ImageBackground source={beachImage} resizeMode="cover" className="flex-1">
-				<LinearGradient className="flex-1" colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
-					<SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+				<AppGradient colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+					<SafeAreaView className="flex flex-1 px-1 justify-between">
 						<View>
 							<Text className="text-center text-white font-bold text-4xl">
 								Simple Meditation
@@ -20,16 +23,15 @@ const App = () => {
 								Simplifying Mediation for Everyone!
 							</Text>
 						</View>
-
 						<View>
 							<CustomButton
-								onPress={() => console.log('pressed')}
+								onPress={() => router.push('/nature-meditate')}
 								title="Get Started"
 							/>
 						</View>
 						<StatusBar style="light" />
 					</SafeAreaView>
-				</LinearGradient>
+				</AppGradient>
 			</ImageBackground>
 		</View>
 	);
