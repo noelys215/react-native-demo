b698044f9c19826601dead95608961241bed97e4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-09-18 20:21:07 -0400

    feat: Add image background and custom button to main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,38 @@
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import React from 'react';
+import beachImage from '@/assets/meditation-images/beach.webp';
+import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import CustomButton from '@/components/CustomButton';
 
-const index = () => {
+const App = () => {
 	return (
-		<View>
-			<Text>index</Text>
+		<View className="flex-1">
+			<ImageBackground source={beachImage} resizeMode="cover" className="flex-1">
+				<LinearGradient className="flex-1" colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+					<SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+						<View>
+							<Text className="text-center text-white font-bold text-4xl">
+								Simple Meditation
+							</Text>
+							<Text className="text-center text-white text-regular text-2xl mt-3">
+								Simplifying Mediation for Everyone!
+							</Text>
+						</View>
+
+						<View>
+							<CustomButton
+								onPress={() => console.log('pressed')}
+								title="Get Started"
+							/>
+						</View>
+						<StatusBar style="light" />
+					</SafeAreaView>
+				</LinearGradient>
+			</ImageBackground>
 		</View>
 	);
 };
 
-export default index;
+export default App;
