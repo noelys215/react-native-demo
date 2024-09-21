6e7c5c1aced29a6ba2aed709ec5667bb12e12680
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-09-21 19:54:20 -0400

    feat: Update nature-meditate screen with image background and custom button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Slot } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 
 export default function Layout() {
-	return <Slot />;
+	return (
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+		</Stack>
+	);
 }
