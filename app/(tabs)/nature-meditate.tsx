3a9e6f76d83dff31db28986596db46bf64abceca
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-09-20 21:20:44 -0400

    refactor: Update nature-meditate screen with image background and custom button

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, Pressable, ImageBackground } from 'react-native';
 import React from 'react';
+import AppGradient from '@/components/AppGradient';
+import { StatusBar } from 'expo-status-bar';
+import { MEDITATION_DATA } from '@/constants/MeditationData';
+import MEDITATION_IMAGES from '@/constants/meditaion-images';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const NatureMeditate = () => {
 	return (
-		<View className="flex-1 justify-center items-center">
-			<Text>NatureMeditate</Text>
+		<View className="flex-1">
+			<AppGradient colors={['#161B2E', '#0A4D4A', '#766E67']}>
+				<View className="mb-6">
+					<Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
+						Welcome Dude
+					</Text>
+					<Text className="text-indigo-100 text-xl font-medium">
+						Start your meditation practice today.
+					</Text>
+				</View>
+
+				<View>
+					<FlatList
+						data={MEDITATION_DATA}
+						className="mb-20"
+						keyExtractor={(item) => item.id.toString()}
+						showsVerticalScrollIndicator={false}
+						renderItem={({ item }) => (
+							<Pressable
+								onPress={() => console.log('Pressed')}
+								className="h-48 my-3 rounded-md overflow-hidden">
+								<ImageBackground
+									source={MEDITATION_IMAGES[item.id - 1]}
+									resizeMode="cover"
+									className="flex-1 rounded-lg justify-center">
+									<LinearGradient
+										colors={['transparent', 'rgba(0,0,0,0.8)']}
+										className="flex-1 justify-center items-center">
+										<Text className="text-gray-100 text-3xl font-bold text-center">
+											{item?.title}
+										</Text>
+									</LinearGradient>
+								</ImageBackground>
+							</Pressable>
+						)}
+					/>
+				</View>
+			</AppGradient>
+			<StatusBar style="light" />
 		</View>
 	);
 };
