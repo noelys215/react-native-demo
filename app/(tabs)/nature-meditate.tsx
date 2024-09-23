1e9ee24da88d26ce9011d8a1563f46364c319a87
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-09-23 18:12:00 -0400

    refactor: Update nature-meditate screen with image background and custom button

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import AppGradient from '@/components/AppGradient';
 import { StatusBar } from 'expo-status-bar';
 import { MEDITATION_DATA } from '@/constants/MeditationData';
-import MEDITATION_IMAGES from '@/constants/meditaion-images';
+import MEDITATION_IMAGES from '@/constants/meditation-images';
 import { LinearGradient } from 'expo-linear-gradient';
+import { router } from 'expo-router';
 
 const NatureMeditate = () => {
 	return (
@@ -27,7 +28,7 @@ const NatureMeditate = () => {
 						showsVerticalScrollIndicator={false}
 						renderItem={({ item }) => (
 							<Pressable
-								onPress={() => console.log('Pressed')}
+								onPress={() => router.push(`/meditate/${item.id}`)}
 								className="h-48 my-3 rounded-md overflow-hidden">
 								<ImageBackground
 									source={MEDITATION_IMAGES[item.id - 1]}
